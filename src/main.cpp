d619819f617f676ea5f3ff84d34ddb047acdf8b1
MinusGix <MinusGix@gmail.com>
2019-09-24 12:02:47 -0500

    Move main header file includes into code file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
-#include "./main.hpp"
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <optional>
+#include <filesystem>
+
+#include <curses.h>
+
+// ignore errors from these headers since I'm not changing these
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weverything"
+
+#define SOL_ALL_SAFETIES_ON 1
+#include "./sol.hpp"
+#include "./cxxopts.hpp"
+
+#pragma GCC diagnostic pop
+
+#include "./Herix/src/herix.hpp"
+
+#include "./mutil.hpp"
+#include "./window.hpp"
 
 using namespace HerixLib;
 
